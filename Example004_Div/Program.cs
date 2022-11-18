9776f9017690bc5211db82cb46931c7ce1a3a80b
Aleksandra <toropova230@gmail.com>
2022-11-18 10:24:26 +0300

    add Example 004 Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,13 @@
+﻿// int - для целых чисел:
+
+// int numberA = 10;
+// int numberB = 5;
+// Console.WriteLine(numberA / numberB);
+
+
+
+// double - для вещественных чисел (для деления с остатком):
+
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
